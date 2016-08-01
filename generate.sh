bdd683d29397fd0e0ab09397a93d0df01ac1052e
NCrashed <ncrashed@gmail.com>
2016-08-01 19:16:15 +0300

    Update generate script

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 auth-token-swagger-gen swagger.json docs.markdown
 pandoc docs.markdown -o docs.html
-git add swagger.json docs.html
+git add swagger.json docs.html docs.markdown
 git commit -m "Update documentation"
 git push origin gh-pages
